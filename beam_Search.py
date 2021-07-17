4fb17008c2f5718c292ae2c89af815d524ff4908
gal barak <galbarak161@gmail.com>
2021-07-17 11:02:58 +0300

    Added Beam Search

---
+++ beam_Search.py
@@ -0,0 +1,107 @@
+import heapq
+from queue import Queue
+import numpy as np
+import torch
+
+
+class BeamSearchNode(object):
+    def __init__(self, log_prob, tokenized_words):
+        self.log_prob = log_prob
+        self.minus_log_prob = -log_prob
+        self.tokenized_words = tokenized_words
+
+    def __lt__(self, other):
+        return self.minus_log_prob < other.minus_log_prob
+
+
+def beam_search(max_gen_seq_length, model, beam_width, init_words, init_h0, init_c0):
+    # queue to hold the next beam-front
+    queue = Queue()
+
+    # create Min-heap
+    heap = []
+    heapq.heapify(heap)
+
+    softmax = torch.nn.LogSoftmax(dim=2)
+    eos_flag = False
+
+    # Generate 1st words:
+    first_words = model(init_words, "beam_search", init_h0, init_c0)
+    first_words_probabilities = softmax(first_words)
+    vocab_length = first_words_probabilities.shape[2]
+
+    # Create beam search_objects according to scores, and insert to the heap
+    for i in range(vocab_length):
+        tokenized_word = [i]
+        word_prob = first_words_probabilities[0, 0, i]
+        new_node = BeamSearchNode(word_prob, tokenized_word)
+        heapq.heappush(heap, new_node)
+
+    # Grab top BEAM_WIDTH elements to proceed with
+    for i in range(beam_width):
+        node = heapq.heappop(heap)
+        queue.put(node)
+
+    # Perform the rest of the beam-search:
+    for i in range(max_gen_seq_length):
+        # Clean heap:
+        heap = []
+        heapq.heapify(heap)
+
+        # Insert new elements to the heap,
+        # every queue-node which represents current best sentences, holds ~10k elements
+        while not queue.empty():
+            q_node = queue.get()
+
+            # add a Batch-dimension so we can run it on model
+            x = [2] + q_node.tokenized_words
+            x = torch.IntTensor(x)
+            x = torch.unsqueeze(x, 1)
+
+            # Generate the i-th-stage words:
+            ith_words = model(x, "beam_search", init_h0, init_c0)
+            ith_words = ith_words[-1, :, :]  # only the last Y is relevant...
+            ith_words = torch.unsqueeze(ith_words, 0)  # retrieve the 1st dimension lost from previous line ^
+            first_words_probabilities = softmax(ith_words)
+
+            # Create beam search_objects according to scores, and insert to heap
+            for j in range(vocab_length):
+                # adjust current word with previous words
+                tokenized_word = q_node.tokenized_words + [j]
+
+                # adjust current score with previous scores
+                word_prob = q_node.log_prob + first_words_probabilities[0, 0, j]
+
+                new_node = BeamSearchNode(word_prob, tokenized_word)
+                heapq.heappush(heap, new_node)
+
+        # Check stop-case (if most-likely sentence holds <eos> as his last generated word)
+        check_node = heap[0]
+        if check_node.tokenized_words[-1] == 3:
+            eos_flag = True
+            break
+
+        # Grab top BEAM_WIDTH elements to proceed with
+        for j in range(beam_width):
+            node = heapq.heappop(heap)
+            queue.put(node)
+
+    if eos_flag:
+        node = heapq.heappop(heap)
+        final_sentence = node.tokenized_words
+        return final_sentence
+
+    # increase reached a generated sentence of size 50
+    else:
+        best_node = None
+        best_prob = -np.inf
+
+        while not queue.empty():
+            node = queue.get()
+            if node.log_prob > best_prob:
+                best_node = node
+
+        if best_node is None:
+            return node.tokenized_words
+        return best_node.tokenized_words
+
--- words_generator.py
+++ words_generator.py
@@ -1,16 +1,20 @@
 import numpy as np
 import torch
 
+from beam_Search import beam_search
 from predictor_module import MODEL_FILE_NAME, DEVICE
 
 
-def generates_word_based_on_previous(empty_model, vocab, batch_size):
-    # load model parameters
+def generate_words(empty_model, batch_size, vocab, dl_test):
+    MAX_GEN_SEQ_LENGTH = 50
+
     model = empty_model
     if torch.cuda.is_available():
         model.load_state_dict(torch.load(MODEL_FILE_NAME))
     else:
         model.load_state_dict(torch.load(MODEL_FILE_NAME, map_location='cpu'))
+
+    model.eval()
 
     hidden_size = model.hidden_size
     layers = model.num_layers
@@ -20,21 +24,105 @@ def generates_word_based_on_previous(empty_model, vocab, batch_size):
     init_h0 = torch.randn(layers, batch_size, hidden_size).to(DEVICE)
     init_c0 = torch.randn(layers, batch_size, hidden_size).to(DEVICE)
 
-    tokenize_gen_sequences = model(init_words, "teacher_force", init_h0, init_c0)
+    # grab a single batch from Test Set
+    gt_batch = next(iter(dl_test))
+    gt_batch, batch_len = gt_batch.d
+    gt_batch = gt_batch[:, :MAX_GEN_SEQ_LENGTH]
 
-    sequence_counter = 1
-    with open('Generated Sequences.txt', 'w', encoding="utf8") as f:
-        f.write('\nGenerates words based on previous one:\n')
-        for sequence in tokenize_gen_sequences:
-            untokenize_sequence = []
-            f.write(f'\nSentence {sequence_counter}:\n')
-            for token in sequence:
-                word = vocab.itos[token]
-                if word == '<eos>':
-                    break
-                untokenize_sequence.append(word)
+    # Case A: Generates by previous
+    generates_by_previous(MAX_GEN_SEQ_LENGTH, batch_size, init_words, init_h0, init_c0, model, vocab)
 
-            untokenize_sequence = np.array(untokenize_sequence).T
-            f.write(f'{untokenize_sequence}\n')
-            sequence_counter += 1
+    # Case B: Generates by previous
+    generating_by_Ground_Truth(batch_size, gt_batch, init_c0, init_h0, model, vocab)
 
+    # Case C: Generates by Beam-Search
+    init_words = torch.randint(4, len(vocab) - 1, (1, 1)).to(DEVICE)
+    init_h0 = torch.randn(layers, 1, hidden_size).to(DEVICE)
+    init_c0 = torch.randn(layers, 1, hidden_size).to(DEVICE)
+
+    # generating_by_beam_search(MAX_GEN_SEQ_LENGTH, batch_size, model, vocab, init_words, init_h0, init_c0)
+
+
+def generating_by_beam_search(max_gen_seq_length, batch_size, model, vocab, init_words, init_h0, init_c0):
+    print("\nGenerates words based on Beam search: \n")
+    BEAM_WIDTH = 10
+    # Write generated sequences to file
+    with open('generated_sequences.txt', 'a+', encoding="utf8") as f:
+        f.write('\n####################################\n')
+        f.write('Generates words based on Beam search:\n')
+        f.write('#####################################\n')
+        for i in range(batch_size):
+            tokenized_gen_words = beam_search(max_gen_seq_length, model, BEAM_WIDTH, init_words, init_h0, init_c0)
+            print("x")
+            # Untokenize the generated sequence:
+            for j in range(len(tokenized_gen_words)):
+                tokenized_gen_words[j] = vocab.itos[tokenized_gen_words[j]]
+
+            final_seq = " ".join(tokenized_gen_words)
+            f.write(f'\nsentence {i + 1}:\n{final_seq}\n')
+
+
+def generating_by_Ground_Truth(batch_size, gt_batch, init_c0, init_h0, model, vocab):
+    print("\n Generates words based on Ground Truth: \n")
+
+    tokenized_gen_words = gt_batch.tolist()
+
+    # untokenize words:
+    for i in range(len(tokenized_gen_words)):
+        for j in range(batch_size):
+            tokenized_gen_words[i][j] = vocab.itos[tokenized_gen_words[i][j]]
+
+    # Print Ground Truth
+    with open('generated_sequences.txt', 'a+', encoding="utf8") as f:
+        f.write('\n#####################################\n')
+        f.write('Generates words based on Ground Truth:\n')
+        f.write('######################################\n')
+
+        f.write('\n----- Ground Truth sequences: ------\n')
+        tokenized_gen_words = np.array(tokenized_gen_words)
+        tokenized_gen_words = tokenized_gen_words.T
+        for i in range(batch_size):
+            final_seq = np.array2string(tokenized_gen_words[i])
+            final_seq = final_seq.split("<eos>", 1)[0]
+            f.write(f'\nsentence {i + 1}:\n{final_seq}\n')
+
+        # Generate sentences:
+        tokenized_gen_words_scores = model(gt_batch, "generating_by_Ground_Truth", init_c0, init_h0)
+        tokenized_gen_words_scores = tokenized_gen_words_scores[:(tokenized_gen_words_scores.shape[0] - 1), :, :]
+        tokenized_gen_words = torch.argmax(tokenized_gen_words_scores, dim=2)
+        tokenized_gen_words = tokenized_gen_words.tolist()
+
+        # Untokenize words:
+        for i in range(len(tokenized_gen_words)):
+            for j in range(batch_size):
+                tokenized_gen_words[i][j] = vocab.itos[tokenized_gen_words[i][j]]
+
+        # Print Generated sentences:
+        tokenized_gen_words = np.array(tokenized_gen_words)
+        tokenized_gen_words = tokenized_gen_words.T
+        f.write('\n----- Generated sequences: ------\n')
+        for i in range(batch_size):
+            final_seq = np.array2string(tokenized_gen_words[i])
+            f.write(f'\nsentence {i + 1}:\n{final_seq}\n')
+
+
+def generates_by_previous(max_gen_seq_length, batch_size, init_words, init_h0, init_c0, model, vocab):
+    print("\n Generates words based on previous one: \n")
+
+    tokenized_gen_words = model(init_words, "generates_by_previous", init_h0, init_c0)  # (Generated_S=50, B)
+
+    # Untokenize words:
+    for i in range(max_gen_seq_length):
+        for j in range(batch_size):
+            tokenized_gen_words[i][j] = vocab.itos[tokenized_gen_words[i][j]]
+
+    # Write generated sequences to file
+    with open('generated_sequences.txt', 'a+', encoding="utf8") as f:
+        f.write('\n#####################################\n')
+        f.write('Generates words based on previous one:\n')
+        f.write('######################################\n')
+        tokenized_gen_words = np.array(tokenized_gen_words)
+        tokenized_gen_words = tokenized_gen_words.T
+        for i in range(batch_size):
+            final_seq = np.array2string(tokenized_gen_words[i])
+            f.write(f'\nsentence {i + 1}:\n{final_seq}\n')
